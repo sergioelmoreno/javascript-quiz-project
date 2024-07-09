1c8738fc24274c27d763700d6b12f366b7e6d4eb
Sergio del Campo Puerta <sergio@sergioelmoreno.com>
2024-07-09 16:03:24 +0200

    Quiz Classes logic

--- src/question.js
+++ src/question.js
@@ -1,7 +1,21 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
 
-    // 2. shuffleChoices()
+    constructor(text, choices, answer, difficulty) {
+        this.text = text
+        this.choices = choices
+        this.answer = answer
+        this.difficulty = difficulty
+    }
+
+    shuffleChoices() {
+        for (let i = this.choices.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1))
+            // https://stackoverflow.com/a/12646864
+            // [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]]
+            // can't access lexical declaration 'j' before initialization
+            const temp = this.choices[i]
+            this.choices[i] = this.choices[j]
+            this.choices[j] = temp
+        }
+    }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,54 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions
+        this.timeLimit = timeLimit
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
+    // Return a question Object
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex]
+    }
+    // plus 1 to the index to locate the next question index in the Array
+    moveToNextQuestion() {
+        this.currentQuestionIndex++
+    }
+    // Randomize the order of the questions Array
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1))
+            const temp = this.questions[i]
+            this.questions[i] = this.questions[j]
+            this.questions[j] = temp
+        }
+    }
+    // If the answer is correct add 1 point 
+    checkAnswer(answer) {
+        const currentquestion = this.getQuestion()
+        if (answer === currentquestion.answer) {
+            this.correctAnswers++
+        }
+    }
+    // Check if the Quiz is currently active (Between 1 and 3 questions)
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false
+        } else if (this.currentQuestionIndex === this.questions.length) {
+            return true
+        }
+    }
+    // Filter only the questions with the same provided difficulty
+    filterQuestionsByDifficulty(difficulty) {
+        if (difficulty > 0 && difficulty <= 3) {
+            this.questions = this.questions.filter((question) => question.difficulty === difficulty)
+        }
+    }
+    // Calculate the average difficulty (Number) of all the questions  
+    averageDifficulty() {
+        const averageDifficulty = this.questions.reduce((acc, currentQuestion) => {
+            return acc + currentQuestion.difficulty
+        }, 0)
+        return averageDifficulty / this.questions.length
+    }
 }
